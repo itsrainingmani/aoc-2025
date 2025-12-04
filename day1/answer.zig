d916264175b29ad3cc63075ed6298b99f41440e0
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2025-12-04 02:25:43 -0500

    add timings for each part fn call

--- day1/answer.zig
+++ day1/answer.zig
@@ -51,8 +51,25 @@ fn part2(file_input: []const u8) !i32 {
 }
 
 pub fn main() !void {
-    print("Part 1 (test): {d}\n", .{try part1(input_test)});
-    print("Part 1 (real): {d}\n", .{try part1(input)});
-    print("Part 2 (test): {d}\n", .{try part2(input_test)});
-    print("Part 2 (real): {d}\n", .{try part2(input)});
+    var timer = try std.time.Timer.start();
+
+    const p1_test_start = timer.read();
+    const p1_test = try part1(input_test);
+    const p1_test_time = timer.read() - p1_test_start;
+    print("Part 1 (test): {d} ({d:.2}ms)\n", .{ p1_test, @as(f64, @floatFromInt(p1_test_time)) / std.time.ns_per_ms });
+
+    const p1_real_start = timer.read();
+    const p1_real = try part1(input);
+    const p1_real_time = timer.read() - p1_real_start;
+    print("Part 1 (real): {d} ({d:.2}ms)\n", .{ p1_real, @as(f64, @floatFromInt(p1_real_time)) / std.time.ns_per_ms });
+
+    const p2_test_start = timer.read();
+    const p2_test = try part2(input_test);
+    const p2_test_time = timer.read() - p2_test_start;
+    print("Part 2 (test): {d} ({d:.2}ms)\n", .{ p2_test, @as(f64, @floatFromInt(p2_test_time)) / std.time.ns_per_ms });
+
+    const p2_real_start = timer.read();
+    const p2_real = try part2(input);
+    const p2_real_time = timer.read() - p2_real_start;
+    print("Part 2 (real): {d} ({d:.2}ms)\n", .{ p2_real, @as(f64, @floatFromInt(p2_real_time)) / std.time.ns_per_ms });
 }
